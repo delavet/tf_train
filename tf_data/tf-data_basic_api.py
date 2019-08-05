4e136303cc524eda4add87ec4b29b3c672b2365d
delavet <yhang1996@126.com>
2019-08-05 14:34:50 +0800

    learn to save csv dataset

--- tf_data/tf-data_basic_api.py
+++ tf_data/tf-data_basic_api.py
@@ -20,12 +20,38 @@ for module in mpl, np, pd, sklearn, tf, keras:
     print(module.__name__, module.__version__)
 
 #从内存构建的数据集
-dataset = tf.data.Dataset.from_tensor_slice(np.arange(10))
+dataset = tf.data.Dataset.from_tensor_slices(np.arange(10))
 print(dataset)
 for item in dataset:
     print(item)
 """
 dataset 上可能的操作
 1. 重复遍历（epoch）
-2. get batch
+2. get batch (batch size)
 """
+dataset = dataset.repeat(3).batch(7)
+for item in dataset:
+    print(item)
+
+"""
+interleave: 对训练集中的数据进行一定的映射转换
+case：文件dataset -> 具体数据集
+参数：map_fn 映射方法, cycle_length 并行处理数量 and block_length 变换的结果每次取多少个出来，起到了均匀混合数据集的效果
+"""
+
+dataset2 = dataset.interleave(
+    lambda v: tf.data.Dataset.from_tensor_slices(v),
+    cycle_length = 5,
+    block_length = 5
+)
+
+x = np.array([[1, 2], [3, 4], [5, 6]])
+y = np.array(['cat', 'dog', 'fox'])
+dataset3 = tf.data.Dataset.from_tensor_slices((x, y))
+print(dataset3)
+for item_x, item_y in dataset3:
+    print(item_x.numpy(), item_y.numpy())
+
+dataset4 = tf.data.Dataset.from_tensor_slices({"feature": x, "label": y})
+for item in dataset4:
+    print(item['feature'].numpy(), item['label'].numpy())
